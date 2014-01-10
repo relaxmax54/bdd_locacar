4106ede764b9e879a0f3df546b8faae798de4311
Jerry <fabulousjj@hotmail.com>
2014-01-11 00:54:21 +0100

    ajoute les 2 vues

---
+++ vue2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW vue2 AS
+	SELECT v.date_achat , a.id_agence, c.id_categorie
+	FROM VEHICULES v, AGENCES a, CATEGORIES c
+	WHERE YEAR(v.date_achat) = 2008
+	GROUP BY a.id_agence;
--- vue_info_car.sql
+++ vue_info_car.sql
@@ -1,4 +1,4 @@
 CREATE VIEW info_car AS
 	SELECT v.num_immatriculation , v.id_agence, a.id_ville, t.prix_km, c.id_categorie
 	FROM VEHICULES v, AGENCES a, TARIFS t, CATEGORIES c
-	GROUP BY a.id_ville, v.id_agence, v.num_immatriculation,;
+	GROUP BY a.id_ville, v.id_agence, c.id_categorie, v.num_immatriculation, t.prix_km;
