3c34d4469f4d2415eb09a9b6759ebcba03c3b42b
Tony Vilas <tony.vilas.contact@gmail.com>
2014-01-10 17:23:05 +0100

    definition des contraintes de CLES ETRANGERES

--- tp_alter_table.sql
+++ tp_alter_table.sql
@@ -10,31 +10,10 @@ ALTER TABLE CATEGORIES 	ADD CONSTRAINT	categories_id_categorie_pk PRIMARY KEY (i
 ALTER TABLE TARIFS 		ADD CONSTRAINT	tarifs_id_tarif_pk PRIMARY KEY (id_tarif);
 ALTER TABLE COULEURS 	ADD CONSTRAINT	couleurs_id_couleur_pk PRIMARY KEY (id_couleur);
 ALTER TABLE PAYS 		ADD CONSTRAINT	pays_id_pays_pk PRIMARY KEY (id_pays);
-ALTER TABLE VILLES 		ADD CONSTRAINT	ville_id_ville_pk PRIMARY KEY (id_ville);
+ALTER TABLE VILLES 		ADD CONSTRAINT	villes_id_ville_pk PRIMARY KEY (id_ville);
 
 -- Definition des contraintes de CLES ETRANGERES --
-ALTER TABLE DOSSIERS 	ADD 		
-ALTER TABLE CLIENTS 	ADD 
-ALTER TABLE VEHICULES 	ADD
-ALTER TABLE AGENCES 	ADD	
-ALTER TABLE PAIEMENTS 	ADD
-ALTER TABLE MODELES 	ADD 
-ALTER TABLE MARQUES 	ADD 
-ALTER TABLE CATEGORIES 	ADD
-ALTER TABLE TARIFS 		ADD
-ALTER TABLE COULEURS 	ADD
-ALTER TABLE PAYS 		ADD
-ALTER TABLE VILLES 		ADD
-
-
-
-
-
-
-
-
-
--- Table DOSSIERS
+ALTER TABLE DOSSIERS 	ADD (
 	CONSTRAINT	dossiers_id_client_fk FOREIGN KEY (id_client)
 	REFERENCES	CLIENTS(id_client),
 	CONSTRAINT	dossiers_id_ag_res_fk FOREIGN KEY (id_agence_reservation)
@@ -47,111 +26,96 @@ ALTER TABLE VILLES 		ADD
 	REFERENCES	VEHICULES(num_immatriculation),
 	CONSTRAINT	dossiers_id_tarif_fk FOREIGN KEY (id_tarif)
 	REFERENCES	TARIFS(id_tarif) 
-
-	CONSTRAINT	chk_retour_theorique_after_retrait CHECK (date_retour_prevu > date_retrait),
-	CONSTRAINT	chk_retour_after_retrait CHECK (date_retour_effectif > date_retrait),
-	CONSTRAINT	chk_km_positif CHECK (km_arrivee > km_depart),
-	CONSTRAINT	chk_boolean_assurance CHECK (assurance_prise = UPPER('O') or assurance_prise = UPPER('N'))
-
-	);
-
--- Table CLIENTS
-ALTER TABLE CLIENTS ADD (
-
+	);	
+ALTER TABLE CLIENTS 	ADD (
 	CONSTRAINT	clients_id_ville_fk	FOREIGN KEY (id_ville)
-	REFERENCES	VILLE(id_ville)
+	REFERENCES	VILLES(id_ville)
 	);
--- Table VEHICULES
-ALTER TABLE VEHICULES ADD (
-
-	
-	
+ALTER TABLE VEHICULES 	ADD (
 	CONSTRAINT	vehicules_id_modele_fk FOREIGN KEY (id_modele)
 	REFERENCES	MODELES(id_modele),
 	CONSTRAINT	vehicules_id_couleur_fk	FOREIGN KEY (id_couleur)
 	REFERENCES	COULEURS(id_couleur),
 	CONSTRAINT	vehicules_id_agence_fk	FOREIGN KEY (id_agence)
 	REFERENCES	AGENCES(id_agence),
-	);	
-
--- Table AGENCES
-ALTER TABLE AGENCES ADD (
-	
-
-	CONSTRAINT	agences_id_ville_fk	FOREIGN KEY (id_ville)
-	REFERENCES	VILLE(id_ville)
 	);
-
--- Table PAIEMENTS
-ALTER TABLE PAIEMENTS ADD (
-
-	
-	
+ALTER TABLE AGENCES 	ADD	(
+	CONSTRAINT	agences_id_ville_fk	FOREIGN KEY (id_ville)
+	REFERENCES	VILLES(id_ville)
+	);
+ALTER TABLE PAIEMENTS 	ADD (
 	CONSTRAINT	paiements_id_dossier_fk	FOREIGN KEY (id_dossier)
 	REFERENCES	DOSSIERS(id_dossier)
-	CONSTRAINT	chk_paiements_montant_positif	CHECK (montant >= 0)
 	);
-
--- Table MODELES
-ALTER TABLE MODELES ADD (
-	
-
+ALTER TABLE MODELES 	ADD (
 	CONSTRAINT 	modeles_id_marque_fk FOREIGN KEY (id_marque)
-	REFERENCES 	MARQUES(id_marque)
+	REFERENCES 	MARQUES(id_marque),
 	CONSTRAINT	modeles_id_categorie_fk	FOREIGN KEY (id_categorie)
 	REFERENCES	CATEGORIES(id_categorie),
 	);
-
--- Table MARQUES
-ALTER TABLE MARQUES ADD (
-	
-	);
-
--- Table CATEGORIES
-ALTER TABLE CATEGORIES ADD (
-	
-
-	CONSTRAINT	chk_categories_prix_assurance_positif	CHECK (prix_assurance >= 0)
-	);
-	
--- Table TARIFS
-ALTER TABLE TARIFS ADD(
-	
-
+ALTER TABLE TARIFS 		ADD (
 	CONSTRAINT	tarifs_id_categorie_fk	FOREIGN KEY (id_categorie)
-	REFERENCES	CATEGORIES(id_categorie),
-	CONSTRAINT	chk_tarifs_prix_jour_positif	CHECK (prix_jour >= 0)
-	CONSTRAINT	chk_tarifs_prix_km_positif	CHECK (prix_km >= 0)
+	REFERENCES	CATEGORIES(id_categorie)
 	);
-
--- Table COULEURS
-ALTER TABLE COULEURS ADD (
-	
-	);
--- Table PAYS
-ALTER TABLE PAYS ADD (
-	
-	);
--- Table VILLES
-ALTER TABLE VILLES ADD (
-	
-
-	CONSTRAINT	pays_id_pays_fk	FOREIGN KEY	(id_pays)
+ALTER TABLE VILLES 		ADD (	
+	CONSTRAINT	villes_id_pays_fk	FOREIGN KEY	(id_pays)
 	REFERENCES	PAYS(id_pays)
 	);
 
 
 
--- Definition des contraintes de  --
-ALTER TABLE DOSSIERS 	ADD 		
-ALTER TABLE CLIENTS 	ADD 
-ALTER TABLE VEHICULES 	ADD
-ALTER TABLE AGENCES 	ADD	
-ALTER TABLE PAIEMENTS 	ADD
-ALTER TABLE MODELES 	ADD 
-ALTER TABLE MARQUES 	ADD 
-ALTER TABLE CATEGORIES 	ADD
-ALTER TABLE TARIFS 		ADD
-ALTER TABLE COULEURS 	ADD
-ALTER TABLE PAYS 		ADD
-ALTER TABLE VILLES 		ADD
+
+
+
+
+
+
+-- Table DOSSIERS
+	
+
+	CONSTRAINT	chk_retour_theorique_after_retrait CHECK (date_retour_prevu > date_retrait),
+	CONSTRAINT	chk_retour_after_retrait CHECK (date_retour_effectif > date_retrait),
+	CONSTRAINT	chk_km_positif CHECK (km_arrivee > km_depart),
+	CONSTRAINT	chk_boolean_assurance CHECK (assurance_prise = UPPER('O') or assurance_prise = UPPER('N'))
+
+-- Table CLIENTS
+ALTER TABLE CLIENTS ADD (
+	
+-- Table VEHICULES
+ALTER TABLE VEHICULES ADD (
+
+-- Table AGENCES
+ALTER TABLE AGENCES ADD (
+
+-- Table PAIEMENTS
+ALTER TABLE PAIEMENTS ADD (
+
+	CONSTRAINT	chk_paiements_montant_positif	CHECK (montant >= 0)
+
+-- Table MODELES
+ALTER TABLE MODELES ADD (
+
+-- Table MARQUES
+ALTER TABLE MARQUES ADD (
+	
+-- Table CATEGORIES
+ALTER TABLE CATEGORIES ADD (
+	
+	CONSTRAINT	chk_categories_prix_assurance_positif	CHECK (prix_assurance >= 0)
+	);
+	
+-- Table TARIFS
+ALTER TABLE TARIFS ADD(
+
+	CONSTRAINT	chk_tarifs_prix_jour_positif	CHECK (prix_jour >= 0)
+	CONSTRAINT	chk_tarifs_prix_km_positif	CHECK (prix_km >= 0)
+
+-- Table COULEURS
+ALTER TABLE COULEURS ADD (
+
+-- Table PAYS
+ALTER TABLE PAYS ADD (
+
+-- Table VILLES
+ALTER TABLE VILLES ADD (
+
