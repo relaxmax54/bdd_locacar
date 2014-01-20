8f5a418caff1d3c4bdf93ef9bb582048f275fbcb
relaxmax54 <maxime.frieh@yahoo.fr>
2014-01-20 13:52:51 +0100

    correction des UPPER, déclaration de valeurs par défaut pour toutes les containtes non nulles

--- locacar.sql
+++ locacar.sql
@@ -6,12 +6,12 @@
 	id_agence_retour	NUMBER(4),
 	num_immatriculation	VARCHAR2(7),
 	id_tarif		NUMBER(4),
-	date_retrait		DATE CONSTRAINT dos_date_retrait_nn NOT NULL,
-	date_retour_prevu	DATE CONSTRAINT dos_date_retour_prevu_nn NOT NULL,
-	date_retour_effectif	DATE CONSTRAINT dos_date_retour_effectif_nn NOT NULL,
-	km_depart		NUMBER(6) CONSTRAINT dos_km_depart_nn NOT NULL,
-	km_arrivee		NUMBER(6) CONSTRAINT dos_km_arrivee_nn NOT NULL,
-	assurance_prise		CHAR(1) CONSTRAINT dos_assurance_prise_nn NOT NULL,
+	date_retrait		DATE DEFAULT SYSDATE CONSTRAINT dos_date_retrait_nn NOT NULL,
+	date_retour_prevu	DATE DEFAULT SYSDATE CONSTRAINT dos_date_retour_prevu_nn NOT NULL,
+	date_retour_effectif	DATE DEFAULT SYSDATE CONSTRAINT dos_date_retour_effectif_nn NOT NULL,
+	km_depart		NUMBER(6) DEFAULT 0 CONSTRAINT dos_km_depart_nn NOT NULL,
+	km_arrivee		NUMBER(6) DEFAULT 0 CONSTRAINT dos_km_arrivee_nn NOT NULL,
+	assurance_prise		CHAR(1) DEFAULT 'N' CONSTRAINT dos_assurance_prise_nn NOT NULL,
 	montant_remise		NUMBER(6),
 	pourcentage_remise	NUMBER(3) 	
 	);
@@ -19,8 +19,8 @@
 
 CREATE TABLE	CLIENTS (
 	id_client	NUMBER(4),
-	id_ville	VARCHAR2(7)  CONSTRAINT clients_id_ville_nn NOT NULL,
-	nom		VARCHAR2(25) CONSTRAINT clients_nom_nn NOT NULL,
+	id_ville	VARCHAR2(7) DEFAULT 'FR54NAN'  CONSTRAINT clients_id_ville_nn NOT NULL,
+	nom		VARCHAR2(25) DEFAULT 'inconnu' CONSTRAINT clients_nom_nn NOT NULL,
 	prenom		VARCHAR2(25),
 	adresse		VARCHAR2(255)	
 	);
@@ -31,8 +31,8 @@ CREATE TABLE	VEHICULES (
 	id_modele		NUMBER(2),
 	id_couleur		NUMBER(3),	
 	id_agence		NUMBER(4),
-	date_achat		DATE CONSTRAINT vehicules_date_achat_nn NOT NULL,
-	km_parcourus	NUMBER(6) CONSTRAINT vehicules_km_parcourus_nn NOT NULL
+	date_achat		DATE DEFAULT SYSDATE CONSTRAINT vehicules_date_achat_nn NOT NULL,
+	km_parcourus	NUMBER(6) DEFAULT 0 CONSTRAINT vehicules_km_parcourus_nn NOT NULL
 	);
 
 	
@@ -41,15 +41,7 @@ CREATE TABLE	AGENCES (
 	id_ville	VARCHAR2(7),
 	adresse		VARCHAR2(255),
 	num_telephone	VARCHAR2(10),
-	nom_responsable	VARCHAR2(50) CONSTRAINT agences_nom_responsable_nn NOT NULL
-	);
-
-
-CREATE TABLE	PAIEMENTS (
-	id_paiement	NUMBER(4),
-	id_dossier	NUMBER(4),
-	date_paiement	DATE CONSTRAINT paiements_date_paiement_nn NOT NULL,
-	montant		NUMBER(8,2) 
+	nom_responsable	VARCHAR2(50) DEFAULT 'inconnu' CONSTRAINT agences_nom_responsable_nn NOT NULL
 	);
 
 
@@ -57,13 +49,13 @@ CREATE TABLE 	MODELES (
 	id_modele	NUMBER(2),
 	id_marque	VARCHAR2(7),
 	id_categorie	NUMBER(4),
-	nom		VARCHAR2(20) CONSTRAINT modeles_nom_nn NOT NULL
+	nom		VARCHAR2(20) DEFAULT 'inconnu' CONSTRAINT modeles_nom_nn NOT NULL
 	);
 
 
 CREATE TABLE 	MARQUES (
 	id_marque	VARCHAR2(7),
-	nom		VARCHAR2(20) CONSTRAINT marques_nom_nn NOT NULL
+	nom		VARCHAR2(20) DEFAULT 'inconnu' CONSTRAINT marques_nom_nn NOT NULL
 	);
 
 
@@ -79,10 +71,10 @@ CREATE TABLE 	TARIFS (
 	id_tarif	NUMBER(4),
 	id_categorie	NUMBER(4),
 	nom		VARCHAR2(20),
-	prix_jour	NUMBER(6,2) CONSTRAINT tarifs_prix_jour_nn NOT NULL,
-	prix_forfait	NUMBER(6,2) CONSTRAINT tarifs_prix_forfait_nn NOT NULL,
-	km_autorises	NUMBER(6)	CONSTRAINT tarifs_km_autorises_nn NOT NULL,
-	prix_km	NUMBER(6,2)	CONSTRAINT tarifs_prix_km_nn NOT NULL
+	prix_jour	NUMBER(6,2) DEFAULT 0 CONSTRAINT tarifs_prix_jour_nn NOT NULL,
+	prix_forfait	NUMBER(6,2) DEFAULT 0 CONSTRAINT tarifs_prix_forfait_nn NOT NULL,
+	km_autorises	NUMBER(6)	DEFAULT 0 CONSTRAINT tarifs_km_autorises_nn NOT NULL,
+	prix_km	NUMBER(6,2)	DEFAULT 0 CONSTRAINT tarifs_prix_km_nn NOT NULL
 	);
 
 CREATE TABLE	COULEURS (
@@ -108,7 +100,6 @@ ALTER TABLE DOSSIERS 	ADD CONSTRAINT	dos_id_dossier_pk PRIMARY KEY (id_dossier);
 ALTER TABLE CLIENTS 	ADD CONSTRAINT	clients_id_client_pk PRIMARY KEY (id_client);
 ALTER TABLE VEHICULES 	ADD CONSTRAINT	vehicules_num_immat_pk PRIMARY KEY (num_immatriculation);
 ALTER TABLE AGENCES 	ADD CONSTRAINT	agences_id_agence_pk PRIMARY KEY (id_agence);
-ALTER TABLE PAIEMENTS 	ADD CONSTRAINT	paiements_id_paiement_pk PRIMARY KEY (id_paiement);
 ALTER TABLE MODELES 	ADD CONSTRAINT	modeles_id_modele_pk PRIMARY KEY (id_modele);
 ALTER TABLE MARQUES 	ADD CONSTRAINT	marques_id_marque_pk PRIMARY KEY (id_marque);
 ALTER TABLE CATEGORIES 	ADD CONSTRAINT	categories_id_categorie_pk PRIMARY KEY (id_categorie);
@@ -149,10 +140,6 @@ ALTER TABLE AGENCES 	ADD	(
 	CONSTRAINT	agences_id_ville_fk	FOREIGN KEY (id_ville)
 	REFERENCES	VILLES(id_ville)
 	);
-ALTER TABLE PAIEMENTS 	ADD (
-	CONSTRAINT	paiements_id_dossier_fk	FOREIGN KEY (id_dossier)
-	REFERENCES	DOSSIERS(id_dossier)
-	);
 ALTER TABLE MODELES 	ADD (
 	CONSTRAINT 	modeles_id_marque_fk FOREIGN KEY (id_marque)
 	REFERENCES 	MARQUES(id_marque),
@@ -174,11 +161,8 @@ ALTER TABLE DOSSIERS 	ADD (
 	CONSTRAINT	chk_dos_ret_theo_sup_retrait CHECK (date_retour_prevu > date_retrait),
 	CONSTRAINT	chk_dos_ret_sup_retrait CHECK (date_retour_effectif > date_retrait),
 	CONSTRAINT	chk_dos_km_positif CHECK (km_arrivee > km_depart),
-	CONSTRAINT	chk_dos_boolean_assurance CHECK (assurance_prise = UPPER('O') or assurance_prise = UPPER('N'))
+	CONSTRAINT	chk_dos_boolean_assurance CHECK (UPPER(assurance_prise) = 'O' or UPPER(assurance_prise) = 'N')
 	);		
-ALTER TABLE PAIEMENTS 	ADD (
-	CONSTRAINT	chk_paiements_montant_positif	CHECK (montant >= 0)
-	);
 ALTER TABLE CATEGORIES 	ADD (
 	CONSTRAINT	chk_cat_prix_assurance_positif	CHECK (prix_assurance >= 0)
 	);
