16bb9972bde0e3c34e59ac3e8af4eaf4ab4b9a2f
relaxmax54 <maxime.frieh@yahoo.fr>
2014-01-20 19:34:09 +0100

    Vue 2 corrigée, reste les requêtes et les vues à rectifier en conséquence, ajout d'un nouveau véhicule par défaut : peugeot 508 bleue rattachée à Strasbourg

--- requetesSurVues.sql
+++ requetesSurVues.sql
@@ -1,4 +1,14 @@
+/*requêtes sur les vues*/
+
 select num_immatriculation from info_car where lower(nom)='nancy' group by num_immatriculation
 select num_immatriculation from info_car where prix_km>30 group by num_immatriculation
+
+/* ajout d'un nouveau véhicule à partir de la vue ajouteVehicule*/
+INSERT INTO ajouteVehicule ( NUM_IMMATRICULATION, ID_MODELE, ID_COULEUR, ID_AGENCE, DATE_ACHAT, KM_PARCOURUS)
+ VALUES ( 'NV999ZZ', 3, 3, 4);
+ /*insertion d'un nouvel élément
+	 * : une peugeot 508 de couleur bleue rattachée à l'agence strasbourgeoise, avec les valeurs des deux dernières colonnes par défaut)
+*/	
+
 
 /
