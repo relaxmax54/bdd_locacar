efaca6946b358c4d76d6a4697e98022670d90fbb
relaxmax54 <maxime.frieh@yahoo.fr>
2014-01-20 18:39:57 +0100

    Vue 1 corrigée

---
+++ requetesSurVues.sql
@@ -0,0 +1,4 @@
+select num_immatriculation from info_car where lower(nom)='nancy' group by num_immatriculation
+select num_immatriculation from info_car where prix_km>30 group by num_immatriculation
+
+/
--- vue_info_car.sql
+++ vue_info_car.sql
@@ -1,4 +1,9 @@
-CREATE VIEW info_car AS
-	SELECT v.num_immatriculation , v.id_agence, a.id_ville, t.prix_km, c.id_categorie
-	FROM VEHICULES v, AGENCES a, TARIFS t, CATEGORIES c
-	GROUP BY a.id_ville, v.id_agence, c.id_categorie, v.num_immatriculation, t.prix_km;
+CREATE OR REPLACE VIEW info_car AS
+	SELECT v.num_immatriculation, v.id_agence, vi.nom, t.prix_km, c.id_categorie
+	FROM VILLES vi, AGENCES a, VEHICULES v, MODELES mo, CATEGORIES c, TARIFS t
+	WHERE vi.id_ville=a.id_ville
+	AND a.id_agence=v.id_agence
+	AND v.id_modele=mo.id_modele
+	AND mo.id_categorie=c.id_categorie
+	AND c.id_categorie=t.id_categorie
+/
