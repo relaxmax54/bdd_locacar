ab97344afee2b350d4d1540ba58376bdfd942195
relaxmax54 <maxime.frieh@yahoo.fr>
2014-01-21 08:17:07 +0100

    Première procédure

--- fonctions.sql
+++ fonctions.sql
@@ -1,16 +1,27 @@
 CREATE OR REPLACE FUNCTION montantLocation(modeleVehicule IN NUMBER,joursDeLocation IN NUMBER) RETURN NUMBER IS
-prixLocation NUMBER;
+
+DECLARE
+prix tarifs%ROWTYPE;
+prixLocation NUMBER :=0;
+
 BEGIN
 	IF (modeleVehicule IS NOT NULL) THEN
-		select t.prix_jour INTO prixLocation
+		select * INTO prix
 		from tarifs t, categories c, modeles mo
 		where t.id_categorie=c.id_categorie
 		and c.id_categorie=mo.id_categorie
 		and mo.id_modele=modeleVehicule;
-		IF joursDeLocation>0 THEN
-			prixLocation:=prixLocation*joursDeLocation;
+		IF (joursDeLocation>0) THEN
+			IF (MOD(joursDeLocation,7)>0)
+				prixLocation:=prixLocation*MOD(joursDeLocation,7)*prix.prix_jour;
+			ENDIF;
+			joursDeLocation:=joursDeLocation-MOD(joursDeLocation,7);
+			prixLocation=joursDeLocation*prix.prix_forfait;
 		END IF;
 	END IF;
+
 	RETURN prixLocation;
+
 END;
+
 /
