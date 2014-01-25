7b3042fd24f3b3870338c3e14a3d56863aa943e5
Tony Vilas <tony.vilas.contact@gmail.com>
2014-01-25 08:58:23 +0100

    Useless commit

---
+++ init.sql
@@ -0,0 +1,15 @@
+drop cascade
+
+drop table dossiers;
+drop table clients;
+drop table vehicules;
+drop table agences;
+drop table paiements;
+drop table modeles;
+drop table marques;
+drop table categories;
+drop table tarifs;
+drop table couleurs;
+drop table pays;
+drop table villes;
+
