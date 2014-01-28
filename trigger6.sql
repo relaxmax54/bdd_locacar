ecb723296b825f25eae6f2e9904b2cda1d690405
Tony Vilas <tony.vilas.contact@gmail.com>
2014-01-28 16:24:50 +0100

    mise à niveau

--- trigger6.sql
+++ trigger6.sql
@@ -11,8 +11,10 @@ BEGIN
 	SELECT	d.km_arrivee, v.num_immatriculation 
 	INTO	kmarr, immat
 	FROM	VEHICULES v, DOSSIERS d
-	WHERE	d.num_immatriculation = v.num_immatriculation;
-
+	WHERE	d.num_immatriculation = v.num_immatriculation
+	AND		d.id_agence_retrait = v.id_agence
+	AND		d.km_depart = v.km_parcourus;
+	
 	UPDATE VEHICULES 
 	SET km_parcourus = kmarr
 	WHERE num_immatriculation = immat;
