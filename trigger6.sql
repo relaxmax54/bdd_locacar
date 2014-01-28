59300dec4577d48e349804a6635487946c496426
Tony Vilas <tony.vilas.contact@gmail.com>
2014-01-28 17:50:56 +0100

    Derniere version du trigger 6. A tester en base Oracle.

--- trigger6.sql
+++ trigger6.sql
@@ -3,20 +3,8 @@ après mise à jour du kilométrage effectif du véhicule au retour d’une loca
 
 CREATE OR REPLACE TRIGGER update_km_parcourus
 AFTER UPDATE OF km_arrivee ON DOSSIERS
-DECLARE
-	immat	VEHICULES.num_immatriculation%TYPE;
-	kmarr	DOSSIERS.km_arrivee%TYPE;
-
 BEGIN
-	SELECT	d.km_arrivee, v.num_immatriculation 
-	INTO	kmarr, immat
-	FROM	VEHICULES v, DOSSIERS d
-	WHERE	d.num_immatriculation = v.num_immatriculation
-	AND		d.id_agence_retrait = v.id_agence
-	AND		d.km_depart = v.km_parcourus;
-	
-	UPDATE VEHICULES 
-	SET km_parcourus = kmarr
-	WHERE num_immatriculation = immat;
+	UPDATE VEHICULES
+	SET km_parcourus = :NEW.km_arrivee;
 END;
 /
