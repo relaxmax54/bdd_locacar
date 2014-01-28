e74eaf403b9fe74272f42dca84bb2f8df1c53b33
relaxmax54 <maxime.frieh@yahoo.fr>
2014-01-29 00:51:16 +0100

    ajout fonction 1

--- fonctions.sql
+++ fonctions.sql
@@ -1,26 +1,28 @@
-create or replace function calculTarif(dump in number)return number is
-reponse number:=0;
-begin
-return reponse;
-end;
-/
-create or replace function devis(modeleVehicule in modeles.id_modele%type,joursLocation in number)return number is
-cursor modele is select id_categorie from modeles where id_modele=modeleVehicule;
-cursor categorie is select id_categorie from categories;
-cursor tarif is select id_categorie,prix_jour,prix_forfait from tarifs;
-reponse number :=0;
-begin
-for record_modele in modele loop
-	for record_categorie in categorie loop
-		for record_tarif in tarif loop
-			if(record_modele.id_categorie=record_categorie.id_categorie and 
-
-record_categorie.id_categorie=record_tarif.id_categorie) then
-				select calculTarif(1) into reponse from dual;
-			end if;
-		end loop;
-	end loop;
-end loop;	
-return reponse;
-end;
+CREATE OR REPLACE FUNCTION montantLocation(modeleVehicule IN NUMBER,joursDeLocation IN NUMBER) RETURN NUMBER IS
+prixjour tarifs.prix_jour%type;
+prixforfait tarifs.prix_forfait%type;
+prixLocation number:=0;
+BEGIN
+IF (joursDeLocation>0) THEN
+	select t.prix_jour INTO prixjour
+	from tarifs t, categories c, modeles mo
+	where t.id_categorie=c.id_categorie
+	and c.id_categorie=mo.id_categorie
+	and mo.id_modele=modeleVehicule
+	and rownum=1
+	order by t.id_tarif;
+	select t.prix_forfait into prixforfait
+	from tarifs t, categories c, modeles mo		
+	where t.id_categorie=c.id_categorie
+	and c.id_categorie=mo.id_categorie
+	and mo.id_modele=modeleVehicule
+	and rownum=1
+	order by t.id_tarif;
+	IF (MOD(joursDeLocation,7)>0) then
+		prixLocation:=MOD(joursDeLocation,7)*prixjour;
+	END IF;
+	prixLocation:=prixLocation+((joursDeLocation-MOD(joursDeLocation,7))/7)*prixforfait;
+END IF;
+RETURN prixLocation;
+END;
 /
