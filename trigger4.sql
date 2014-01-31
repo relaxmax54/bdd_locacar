98426d830d23fe906bd465f541809902534f830b
relaxmax54 <maxime.frieh@yahoo.fr>
2014-01-31 11:01:44 +0100

    trigger4

--- trigger4.sql
+++ trigger4.sql
@@ -4,24 +4,22 @@ toutes les locations dont la durée est > 7 jours.*/
 CREATE OR REPLACE TRIGGER update_loc_sup_semaine
 AFTER UPDATE OF date_retour_effectif ON DOSSIERS
 FOR EACH ROW
+WHEN ((NEW.date_retour_effectif-OLD.date_retrait + 1)> 7)
 DECLARE
-	PRAGMA AUTONOMOUS_TRANSACTION;
+CURSOR curdos is
+(SELECT 	d.id_dossier iddossier, c.nom nom, c.prenom prenom, ma.nom marque, mo.nom modele
+FROM	DOSSIERS d, CLIENTS c, MODELES mo, MARQUES ma, VEHICULES v
+WHERE	c.id_client = d.id_client
+AND	d.num_immatriculation = v.num_immatriculation
+AND	v.id_modele = mo.id_modele
+AND	mo.id_marque = ma.id_marque
+AND id_dossier = :OLD.id_dossier);
 BEGIN
-	IF date_retour_effectif-date_retrait + 1 > 7
-	THEN (
-		INSERT INTO LOC_SUP_SEMAINE (id_dossier, nom_client, prenom_client, nom_marque, modele, date_du_jour)
-		VALUES (	(SELECT 	d.id_dossier, c.nom, c.prenom, ma.nom, mo.nom
-					FROM	DOSSIERS d, CLIENTS c, MODELES mo, MARQUES ma, VEHICULES v
-					WHERE	c.id_client = d.id_client
-					AND		d.num_immatriculation = v.num_immatriculation
-					AND		v.id_modele = mo.id_modele
-					AND		mo.id_marque = ma.id_marque),
-				SYSDATE);
-		WHERE id_dossier = :OLD.id_dossier;);
-	END IF;
-	COMMIT;
+for boucle in curdos loop
+INSERT INTO LOC_SUP_SEMAINE (id_dossier, nom_client, prenom_client, nom_marque, modele, date_du_jour)
+		VALUES (boucle.iddossier,boucle.nom,boucle.prenom,boucle.marque,boucle.modele,SYSDATE);
+end loop;
 END;
 /
-
 
 
