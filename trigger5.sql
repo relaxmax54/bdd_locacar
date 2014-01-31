5e9cf4d939a8ee09253b67a67dd44ad3ef7e0fd4
relaxmax54 <maxime.frieh@yahoo.fr>
2014-01-31 11:26:55 +0100

    trigger5

--- trigger5.sql
+++ trigger5.sql
@@ -3,13 +3,16 @@ le nombre cumulé de jours de location de chaque véhicule. */
 
 CREATE OR REPLACE TRIGGER update_count_days_loc
 AFTER UPDATE OF date_retour_effectif ON DOSSIERS
+FOR EACH ROW
 DECLARE
-	PRAGMA AUTONOMOUS_TRANSACTION;
+CURSOR curdos is
+(select v.compteur,v.num_immatriculation immat,d.date_retour_effectif dre,d.date_retrait dr from vehicules v, dossiers d where d.num_immatriculation=v.num_immatriculation);
 BEGIN
+for boucle in curdos loop
 	UPDATE VEHICULES
-	SET compteur = compteur + (date_retour_effectif - date_retrait);
-	WHERE num_immatriculation = :OLD.num_immatriculation;
-	COMMIT;
+	SET compteur = boucle.compteur + (boucle.dre - boucle.dr)
+	WHERE boucle.immat = :OLD.num_immatriculation;
+end loop;
 END;
 /
 
